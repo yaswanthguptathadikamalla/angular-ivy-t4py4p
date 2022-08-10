7564944f4804b5cdcfb35f119a5c377103d281b2
yaswanthguptathadikamalla <yaswanthgupta31@gmail.com>
2022-08-10 12:37:38 +0530

    3 files modified, 6 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'my-app';
+
+  constructor(private router: Router) {}
+
+  onButtonClicked(): void {
+    this.router.navigateByUrl('products');
+  }
 }
